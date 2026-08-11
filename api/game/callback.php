3fe51fc00b27a83d2068e710790e375d0968e90d
Sukanto Mondol <2021909@iub.edu.bd>
2026-08-11 23:53:47 +0600

    Add incoming request logging to api/game/callback.php to capture Gamblly callback payload

--- api/game/callback.php
+++ api/game/callback.php
@@ -8,8 +8,18 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization, X-API-Key, X-Agency-Uid');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 
+// LOG every incoming request so we can see what Gamblly sends
+$_rawBody   = file_get_contents('php://input');
+$_logEntry  = '[' . date('Y-m-d H:i:s') . '] GAMBLLY_CALLBACK_RECEIVED '
+    . 'method=' . $_SERVER['REQUEST_METHOD'] . ' '
+    . 'ip=' . ($_SERVER['HTTP_X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR'] ?? 'unknown') . ' '
+    . 'raw=' . $_rawBody . ' '
+    . 'post=' . json_encode($_POST)
+    . PHP_EOL;
+@file_put_contents(__DIR__ . '/../game_api_debug.log', $_logEntry, FILE_APPEND);
+
 if (function_exists('game_api_start_error_logging')) { game_api_start_error_logging(); }
 if (function_exists('game_api_ensure_schema')) { game_api_ensure_schema($conn, false); }
 
 gamblly_api_handle_callback($conn);
-?>
+
